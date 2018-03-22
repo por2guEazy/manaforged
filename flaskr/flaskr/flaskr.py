1d6b1da5d2a1560396c3bf7959502a83befbe969
Eli Sandine <elisandine@Elis-MacBook-Air.local>
2018-03-22 16:17:28 -0700

    Changed the server code to render static pages for now. Moved *.html files to templates directory.

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -3,11 +3,27 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def render():
-    return "Cooooool beans"
+def index():
+    return render_template('index.html')
 
-@app.route('/<string:page_name>/')
-def static_page(page_name):
-    return render_template('%s.html' % page_name)
-if __name__=='__main__':
-    app.run()
+
+@app.route('/about/')
+def about():
+    return  render_template('./about.html')
+
+
+@app.route('/try')
+def try_it():
+    return  render_template('./try.html')
+
+
+@app.route('/how')
+def how():
+    return  render_template('./how.html')
+
+@app.route('/examples')
+def examples():
+    return  render_template('./examples.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
